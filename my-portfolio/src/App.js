2f00b9d1e61b60bcb0d1d1888486ca1a5fa39174
Aditi <addynair6@gmail.com>
2024-11-11 23:07:22 +0530

    Added title

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HeroSection from './components/Herosection';
 import WorkSection from './components/worksection';
 import AboutSection from './components/aboutsection';
 import SocialLinks from './components/sociallinks';
 import ViewProject from './components/ViewProject';
+
 function App() {
   const [selectedProject, setSelectedProject] = useState(null); 
+
   const handleViewProject = (project) => {
     setSelectedProject(project); 
   };
 
+  const handleCloseProject = () => {
+    setSelectedProject(null); 
+  };
+
+  
+  useEffect(() => {
+    document.title = selectedProject ? selectedProject.title : "Aditi Nair"; 
+  }, [selectedProject]);
+
   return (
     <div className="App">
       {selectedProject ? (
-        <ViewProject project={selectedProject} /> 
+        <ViewProject project={selectedProject} onClose={handleCloseProject} /> 
       ) : (
         <>
           <HeroSection />
